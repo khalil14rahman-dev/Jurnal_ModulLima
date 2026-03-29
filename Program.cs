d2add21ef8d4935d513fa76dd61874a29dba7863
khalilgtgbgtkhalilgtg@gmail.com <khaaleell>
2026-03-29 19:22:30 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Penjumlahan
+{
+    /// <summary>
+    /// Method Generic untuk menjumlahkan tiga angka.
+    /// NIM akhiran 3 menggunakan tipe data double.
+    /// </summary>
+    public void JumlahTigaAngka<T>(T angka1, T angka2, T angka3)
+    {
+        // Menggunakan dynamic agar bisa melakukan operasi '+' pada tipe generic
+        dynamic a = angka1;
+        dynamic b = angka2;
+        dynamic c = angka3;
+        dynamic hasil = a + b + c;
+        Console.WriteLine($"Hasil penjumlahan: {hasil}");
+    }
+}
+
